9dc7958ac8cb917cdbed75279e2581eadc9c5aaa
Kenneth Rungstroem Larsen <rungstroem@hotmail.com>
2021-03-23 21:03:23 +0100

    Tried something new

--- src/depreciated/KernelModule.c
+++ src/depreciated/KernelModule.c
@@ -9,9 +9,9 @@
 #include <asm/uaccess.h>
 #include <linux/version.h>
 #include <linux/types.h>
-#include <linux/i2c.h>
 #include <linux/delay.h>	// Used for msleep() command
 
+#include "I2C.h"
 #include "cmdInterpreter.h"
 
 //#####################################################
@@ -19,6 +19,7 @@
 MODULE_AUTHOR("Kenneth R Larsen");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("A driver for an I2C enabled robot");
+MODULE_DEVICE_TABLE(i2c, my_id_table);
 
 #define DEVICE_NAME "I2CKernelModule"
 #define DRIVER_NAME "I2CDriver"
@@ -32,68 +33,28 @@ MODULE_DESCRIPTION("A driver for an I2C enabled robot");
 static struct cdev c_dev;
 static dev_t dev;
 static struct class *deviceFileClass;
-int init_result;
 int deviceOpen = 0;
 char Message[BUF_LEN];
 char *Message_Ptr;
 bool cmdIdentified;
 //#####################################################
 
-//#####################################################
-// Global variables for I2C
-#define I2C_BUS_AVAILABLE 1
-#define SLAVE_DEVICE_NAME "MPU6050"
-#define SLAVE_DEVICE_ADDRESS 0x68
-
-static struct i2c_adapter * my_i2c_adapter = NULL;
-static struct i2c_client * my_i2c_client = NULL;
-
-//#####################################################
-// I2C structs for general device information
-// Supported devices
-static struct i2c_device_id my_id_table[] = {
-	{ SLAVE_DEVICE_NAME, 0},
-	{},	//Has to be terminated with a 0 entry - some kernel querk...
-};
-
-MODULE_DEVICE_TABLE(i2c, my_id_table);
-
-// Hooks to manage devices
-static struct i2c_driver my_i2c_driver = {
-	.driver = {
-		.name = SLAVE_DEVICE_NAME,
-		.owner = THIS_MODULE,
-	},
-	.id_table = my_id_table,
-
-};
-
-static struct i2c_board_info my_i2c_board_info = {
-	I2C_BOARD_INFO(SLAVE_DEVICE_NAME, SLAVE_DEVICE_ADDRESS)
-};
-
-// ###############################################################################################
-// I2C read and write commands
-int I2C_read_special(unsigned char *buf){
-	if(strcmp("getID", Message)){
-		*buf = (char)i2c_smbus_read_byte_data(my_i2c_client, 0x75);
-		return 0;
-	}else{
-		return -1;	//Command didn't match
-	}
+// #############################################################################################
+// All below is reading and writing to the /dev/I2Cdriver file
+static int dev_open(struct inode *inodep, struct file *filep){
+	if(deviceOpen) return -EBUSY;
+	deviceOpen++;
+	Message_Ptr = Message;
+	try_module_get(THIS_MODULE);
+	printk(KERN_INFO "I2CKernelModule opened");
+	return 0;
 }
 
-void I2C_read_data(unsigned char *outBuf, unsigned int len){
-	while(i2c_master_recv(my_i2c_client, outBuf, len) < 1){
-		//Wait for i2c_master_receive to return positive value
-	}
-	return;
-}
-
-int I2C_write_data(unsigned char *buf, unsigned int len){
-	int ret = -1;
-	ret = i2c_master_send(my_i2c_client, buf, len);
-	return ret;
+static int dev_release(struct inode *inodep, struct file *filep){
+	deviceOpen--;
+	module_put(THIS_MODULE);
+	printk(KERN_INFO "I2CKernelModule closed");
+	return 0;
 }
 
 // Called when reading from the device [cat /dev/I2CDriver]
@@ -105,10 +66,6 @@ static ssize_t dev_read(struct file *filep, char *userBuffer, size_t len, loff_t
 	char outMessage;
 	//Print for debugging
 	printk(KERN_INFO "Read from device Entered");
-
-	// Read from I2C
-	//I2C_read_special(Message);
-	//Message_Ptr = Message[0];		//Sets the pointer to the start of the message
 	
 	if(cmdIdentified){
 		D = &data;
@@ -179,31 +136,6 @@ static ssize_t dev_write(struct file *filep, const char *userBuffer, size_t len,
 	return i;
 }
 
-// I2C init and remove functions prototypes
-int initI2C(void);
-void unregisterI2C(void);
-
-// Device file init and remove function prototypes
-int initDeviceFile(void);
-void unregisterDeviceFile(void);
-
-// Runs when module is opened
-static int dev_open(struct inode *inodep, struct file *filep){
-	if(deviceOpen) return -EBUSY;
-	deviceOpen++;
-	Message_Ptr = Message;
-	try_module_get(THIS_MODULE);
-	printk(KERN_INFO "I2CKernelModule opened");
-	return 0;
-}
-// Runs when module is closed
-static int dev_release(struct inode *inodep, struct file *filep){
-	deviceOpen--;
-	module_put(THIS_MODULE);
-	printk(KERN_INFO "I2CKernelModule closed");
-	return 0;
-}
-
 static struct file_operations fops={
 	.owner = THIS_MODULE,
 	.open = dev_open,
@@ -212,16 +144,25 @@ static struct file_operations fops={
 	.release = dev_release,
 };
 
+int init_device_file(void);
+
 // Runs when module is inserted in kernel (insmod)
-int init_module(void){	
+int init_module(void){
+	int I2CReturnVal;
+	int deviceFileVal;
+
 	// Initialize I2C adaptor and client
-	if(initI2C() < 0){
+	I2CReturnVal = initI2C();
+	if(I2CReturnVal == -1){
 		return -1;
 	}
-	// Initializes the device file
-	if(initDeviceFile() < 0){
+
+	// Initialize the device file
+	deviceFileVal = init_device_file();
+	if(deviceFileVal == -1){
 		return -1;
 	}
+
 	// This retuns if init executes okay. 
 	printk(KERN_INFO "I2CKernelModule inserted\n");
 	return 0;
@@ -232,47 +173,17 @@ void cleanup_module(void){
 	// Used to release the I2C interface
 	unregisterI2C();
 
-	unregisterDeviceFile();
+	// Used to remove driver from /dev
+	//unregister_chrdev(major, DEVICE_NAME);
+	cdev_del( &c_dev );
+	device_destroy( deviceFileClass, dev );
+	class_destroy( deviceFileClass );
+	unregister_chrdev_region(dev, 1);
 
 	printk(KERN_INFO "I2CKernelModule removed\n");
 }
 
-
-
-
-// ###############################################################################################
-// I2C init and remove functions
-int initI2C(void){
-	int ret = -1;
-	
-	// Create adaptor to get access to I2C bus
-	my_i2c_adapter = i2c_get_adapter(I2C_BUS_AVAILABLE);
-	if(my_i2c_adapter != NULL){
-		my_i2c_client = i2c_new_device(my_i2c_adapter, &my_i2c_board_info);
-		if(my_i2c_client != NULL){
-			if(i2c_add_driver(&my_i2c_driver) != -1){
-				ret = 0;
-			} else{
-				printk("Can't add i2c driver\n");
-			}
-		}
-		i2c_put_adapter(my_i2c_adapter);
-	}
-	return ret;
-}
-void unregisterI2C(void){
-	i2c_unregister_device(my_i2c_client);	// Removes my_i2c_client from adapter
-	printk(KERN_INFO "I2C_client removed");
-	i2c_del_adapter(my_i2c_adapter);		// Unregisters the I2C_adapter
-	printk(KERN_INFO "I2C_adapter deleted");
-	i2c_del_driver(&my_i2c_driver);		// Remove I2C_driver from kernel
-	printk(KERN_INFO "I2C driver deleted");
-}
-
-
-// ############################################################################################
-// Device file init and destroy functions
-int initDeviceFile(void){
+int init_device_file(void){
 	// Allocate device nr.
 	if( (alloc_chrdev_region(&dev, 0, 1, DRIVER_NAME)) < 0 ){
 		printk( KERN_ALERT "I2CKernelModule registration failed\n" );
@@ -287,7 +198,7 @@ int initDeviceFile(void){
 	}
 	
 	//Create device file
-	if( device_create(deviceFileClass, NULL, dev, NULL, "I2CDriver") == NULL ){
+	if( (device_create(deviceFileClass, NULL, dev, NULL, "I2CDriver")) == NULL ){
 		printk(KERN_ALERT "Device creation failed");
 		class_destroy(deviceFileClass);
 		unregister_chrdev_region(dev,1);
@@ -298,23 +209,14 @@ int initDeviceFile(void){
 	cdev_init(&c_dev, &fops);
 
 	// Register device to kernel
-	if( cdev_add( &c_dev, dev, 1) == -1){
+	if( (cdev_add( &c_dev, dev, 1)) == -1){
 		printk(KERN_ALERT "Device addition failed");
 		device_destroy( deviceFileClass, dev );
 		class_destroy( deviceFileClass );
 		unregister_chrdev_region(dev, 1);
 		return -1;
 	}
-
+	// If everything went well - return 0
 	return 0;
-}
-
-void unregisterDeviceFile(void){
-	// Used to remove driver from /dev
-	//unregister_chrdev(major, DEVICE_NAME);
-	cdev_del( &c_dev );
-	device_destroy( deviceFileClass, dev );
-	class_destroy( deviceFileClass );
-	unregister_chrdev_region(dev, 1);
 
 }
